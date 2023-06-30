acddbf07746ff0f24e4313b4297905c1339e2818
whodef <tatyana@selyuk.com>
2023-06-30 20:33:52 +0300

    feat: Update project

--- app/services/constants.py
+++ app/services/constants.py
@@ -1,42 +1,71 @@
-"""Сборник строк и прочих постоянных значений.
-"""
-# to datetime.isoformat
+
+# For core.users
+
+JWT_LIFE_TIME = 60 * 60
+
+
+# For datetime.isoformat
+
 TIMESPEC = 'seconds'
 
-# for core.users
-JWT_LIFE_TIME = 60 * 60   # seconds
 
-USER_IS_SIGNED = 'Пользователь был зарегистрирован'
+# For endpoints `api.charity_projects`
 
-# names for endpoints in api.charity_projects
 GET_ALL_CHARITY_PROJECTS = 'Просмотреть все благотворительные проекты'
+
 CREATE_CHARITY_PROJECTS = 'Создать новый благотворительный проект'
+
 UPDATE_CHARITY_PROJECT = 'Изменить благотворительный проект'
+
 DELETE_CHARITY_PROJECTS = 'Удалить благотворительный проект'
 
-# names for endpoints in api.donations
+
+# For endpoints `api.donations`
+
 GET_ALL_DONATIONS = 'Просмотреть все пожертвования'
-CREATE_DONATION = 'Добавить пожертвование'
+
 GET_MY_DONATIONS = 'Просмотреть все мои пожертвования'
 
-# names for endpoints in api.google
+CREATE_DONATION = 'Добавить пожертвование'
+
+
+# For endpoints `api.google`
+
 GET_REPORT_TO_GOOGLE = 'Добавить данные из БД в Google-таблицу'
 
-# for googlesheets
-TABLE_NAME = 'Отчеты QRkot'
+
+# For googlesheets
+
+TABLE_NAME = 'Отчеты по проекту QRkot'
+
 SHEET_NAME_RATING_SPEED_CLOSING = 'Рейтинг проектов по скорости закрытия'
 
-# error messages
-ERR_LEN_PASSWORD = 'Password should be at least 3 characters'
-ERR_EMAIL_IN_PASSWORD = 'Пароль содержит Ваш e-mail!'
+
+# For Error Messages
+
 ERR_NO_DELETE_USER = 'Удаление пользователей запрещено!'
 
-ERR_NAME_EXIST = 'Проект с таким именем уже существует!'
-ERR_PROJECT_CLOSED = 'Закрытый проект нельзя редактировать!'
-ERR_FULL_AMOUNT = 'Введённая сумма превышает уже инвестированную!'
-ERR_HAS_INVEST = 'Удаление запрещено! В проект уже внесено %s денег'
+ERR_LEN_PASSWORD = 'Password should be at least 3 characters'
 
-ERR_NOT_FOUND = 'Объект с `id=%s` не найден!'
-ERR_NO_TABLE_FIELD = 'Указанное поле `%s` отсутствует в таблице!'
-ERR_BASE_ANY = 'Ошибка при соединении с БД!'
+ERR_EMAIL_IN_PASSWORD = 'Пароль содержит Ваш e-mail!'
+
+ERR_NAME_EXIST = 'Проект с таким именем уже существует!'
+
+ERR_FULL_AMOUNT = 'Введённая сумма превышает уже инвестированную!'
+
+ERR_HAS_INVEST = 'Удаление запрещено! В проект уже пожертвовано %s'
+
+ERR_PROJECT_CLOSED = 'Закрытый проект нельзя редактировать!'
+
+ERR_NOT_FOUND = 'Объект с таким id не найден.'
+
+ERR_NO_TABLE_FIELD = 'Указанное поле отсутствует в таблице!'
+
 ERR_BASE_INTEGRITY = 'Попытка записи некорректных данных в БД!'
+
+ERR_BASE_ANY = 'Ошибка при соединении с БД!'
+
+
+# Other Messages
+
+USER_IS_SIGNED = 'Пользователь уже зарегистрирован'
