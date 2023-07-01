a173c43a62319517ffe989e1ba1ff7819c928ede
whodef <tatyana@selyuk.com>
2023-07-01 23:47:48 +0300

    feat: Update project

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -1,18 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.validators import (
-    check_charity_project_exists, check_charity_project_fully_invested,
-    check_charity_project_invested_no_money, check_name_duplicate,
-    check_new_full_amount
-)
+from app.api import validators as api_valid
 from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_project_crud
 from app.models import Donation
-from app.schemas.charity_project import (
-    CharityProjectCreate, CharityProjectDB, CharityProjectUpdate
-)
+from app.schemas import charity_project as cp
 from app.services.find_sources import find_sources
 from app.services.invest import invest_money_into_project
 
@@ -21,11 +15,11 @@ router = APIRouter()
 
 @router.get(
     '/',
-    response_model=list[CharityProjectDB],
+    response_model=list[cp.CharityProjectDB],
     response_model_exclude={'close_date'})
 async def get_all_charity_projects(
         session: AsyncSession = Depends(get_async_session)
-) -> list[CharityProjectDB]:
+) -> list[cp.CharityProjectDB]:
     """
     Получение списка всех благотворительных проектов.
 
@@ -43,14 +37,14 @@ async def get_all_charity_projects(
 
 @router.post(
     '/',
-    response_model=CharityProjectDB,
+    response_model=cp.CharityProjectDB,
     response_model_exclude_none=True,
     dependencies=[Depends(current_superuser)]
 )
 async def create_new_charity_project(
-        charity_project: CharityProjectCreate,
+        charity_project: cp.CharityProjectCreate,
         session: AsyncSession = Depends(get_async_session)
-) -> CharityProjectDB:
+) -> cp.CharityProjectDB:
     """
     Создает новый благотворительный проект. Доступно только для SuperUser.
 
@@ -64,7 +58,7 @@ async def create_new_charity_project(
 
         Новый благотворительный проект
     """
-    await check_name_duplicate(charity_project.name, session)
+    await api_valid.check_name_duplicate(charity_project.name, session)
 
     new_project = await charity_project_crud.create(charity_project, session)
 
@@ -83,14 +77,14 @@ async def create_new_charity_project(
 
 @router.patch(
     '/{charity_project_id}',
-    response_model=CharityProjectDB,
+    response_model=cp.CharityProjectDB,
     dependencies=[Depends(current_superuser)]
 )
 async def partially_update_charity_project(
         charity_project_id: int,
-        obj_in: CharityProjectUpdate,
+        obj_in: cp.CharityProjectUpdate,
         session: AsyncSession = Depends(get_async_session)
-) -> CharityProjectDB:
+) -> cp.CharityProjectDB:
     """
     Редактирует существующий благотворительный проект.
 
@@ -110,16 +104,18 @@ async def partially_update_charity_project(
 
         Обновленный благотворительный проект
     """
-    charity_project = await check_charity_project_exists(
+    charity_project = await api_valid.check_charity_project_exists(
         charity_project_id, session)
 
-    await check_charity_project_fully_invested(charity_project)
+    await api_valid.check_charity_project_fully_invested(charity_project)
 
     if obj_in.name:
-        await check_name_duplicate(obj_in.name, session)
+        await api_valid.check_name_duplicate(obj_in.name, session)
 
     if obj_in.full_amount and charity_project.invested_amount:
-        charity_project = await check_new_full_amount(charity_project, obj_in)
+        charity_project = await api_valid.check_new_full_amount(
+            charity_project, obj_in
+        )
 
     charity_project = await charity_project_crud.update(
         charity_project, obj_in, session
@@ -129,12 +125,12 @@ async def partially_update_charity_project(
 
 @router.delete(
     '/{charity_project_id}',
-    response_model=CharityProjectDB,
+    response_model=cp.CharityProjectDB,
     dependencies=[Depends(current_superuser)])
 async def remove_charity_project(
         charity_project_id: int,
         session: AsyncSession = Depends(get_async_session)
-) -> CharityProjectDB:
+) -> cp.CharityProjectDB:
     """
     Удаляет существующий благотворительный проект.
 
@@ -152,11 +148,17 @@ async def remove_charity_project(
 
         Удаленный благотворительный проект
     """
-    charity_project = await check_charity_project_exists(
+    charity_project = await api_valid.check_charity_project_exists(
         charity_project_id, session
     )
-    await check_charity_project_invested_no_money(charity_project_id, session)
-    await check_charity_project_fully_invested(charity_project)
-    charity_project = await charity_project_crud.remove(charity_project, session)
+    await api_valid.check_charity_project_invested_no_money(
+        charity_project_id, session
+    )
+    await api_valid.check_charity_project_fully_invested(
+        charity_project
+    )
+    charity_project = await charity_project_crud.remove(
+        charity_project, session
+    )
 
     return charity_project
--- app/services/constants.py
+++ app/services/constants.py
@@ -1,17 +1,4 @@
 
-# Error messages
-
-PROJECT_NAME_ALREADY_EXISTS = 'Проект с таким именем уже существует!'
-
-PROJECT_NOT_FOUND = 'Проект не найден'
-
-DELETION_NOT_ALLOWED = 'В проект были внесены средства, не подлежит удалению!'
-
-PATCH_NOT_ALLOWED = 'Закрытый проект нельзя редактировать!'
-
-INVALID_FULL_AMOUNT = 'Нельзя установить новую целевую сумму меньше уже внесенной'
-
-
 # User settings
 
 TOKEN_URL = 'auth/jwt/login'
@@ -28,3 +15,16 @@ DEFAULT_INVESTED_AMOUNT = 0
 CHARITY_PROJECT_NAME_MAX = 100
 
 CHARITY_PROJECT_MIN = 1
+
+
+# Error messages
+
+PROJECT_NAME_ALREADY_EXISTS = 'Проект с таким именем уже существует!'
+
+PROJECT_NOT_FOUND = 'Проект не найден'
+
+DELETION_NOT_ALLOWED = 'В проект были внесены средства, не подлежит удалению!'
+
+PATCH_NOT_ALLOWED = 'Закрытый проект нельзя редактировать!'
+
+INVALID_FULL_AMOUNT = 'Нельзя установить новую целевую сумму меньше уже внесенной'
